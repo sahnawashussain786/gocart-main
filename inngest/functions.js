66f212c3ce482aa701c139334655d3aae1db6257
Sahnawas Hussain <142085109+sahnawashussain786@users.noreply.github.com>
2025-09-17 15:28:50 +0530

    first commit

---
+++ inngest/functions.js
@@ -0,0 +1,20 @@
+import { inngest } from "./client";
+import prisma from "@/lib/prisma";
+
+// inngest function to save user data to the database
+
+export const syncUserCreation = inngest.createFunction(
+    {id: 'sync-user-create'},
+    {event: 'clerk/user.created'},
+    async ({event}) => {
+        const {data} = event
+        await prisma.user.create({
+            data: {
+                id: data.id,
+                email: data.email_addresses[0],
+                name: `${data.first_name} ${data.last_name}`,
+                image: data.image_url,
+            }
+        })
+    }
+)
