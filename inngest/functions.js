6646760aa0c1e109929cb40be053eff0fef0aaa0
Sahnawas Hussain <142085109+sahnawashussain786@users.noreply.github.com>
2025-09-17 15:37:20 +0530

    Add function to update user data in the database

--- inngest/functions.js
+++ inngest/functions.js
@@ -18,3 +18,20 @@ export const syncUserCreation = inngest.createFunction(
         })
     }
 )
+
+// function to update user data in the database
+export const syncUserUpdation = inngest.createFunction(
+    {id : 'sync-user-update'},
+    {event: 'clerk/user.updated'},
+    async ({event}) => {
+        const {data} = event
+        await prisma.user.update({
+            where: {id: data.id,},
+            data: {
+                email: data.email_addresses[0],
+                name: `${data.first_name} ${data.last_name}`,
+                image: data.image_url,
+            }
+        })
+    }
+)
